8a122ae62d6dbd2f54abd3c06c5e24b8ec1a3ee3
Francisco Javier Sanchez Temprano <fsancheztemprano@danielcastelao.org>
2019-10-04 15:35:46 +0200

    07

---
+++ src/AD_07_Productsstream/Main.java
@@ -0,0 +1,46 @@
+package AD_07_Productsstream;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import misc.Res;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Product po1 = new Product("cod1", "parafusos", 3);
+        Product po2 = new Product("cod2", "cravos", 4);
+
+        Product[] productos = {po1, po2};
+
+        File file1 = new File(Res.RES_PATH + "productos.txt");
+        System.out.println(file1.getAbsolutePath());
+
+        try (DataOutputStream out = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(file1)))) {
+            for (Product po : productos) {
+                out.writeUTF(po.getCodigo());
+                out.writeUTF(po.getDescripcion());
+                out.writeDouble(po.getPrecio());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (DataInputStream in = new DataInputStream(new BufferedInputStream(new FileInputStream(file1)))) {
+            while (in.available() > 0) {
+                Product po3 = new Product();
+                po3.setCodigo(in.readUTF());
+                po3.setDescripcion(in.readUTF());
+                po3.setPrecio(in.readDouble());
+                System.out.println(po3.toString());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/AD_07_Productsstream/Product.java
@@ -0,0 +1,55 @@
+package AD_07_Productsstream;
+
+import java.util.StringJoiner;
+
+public class Product {
+    private String codigo;
+    private String descripcion;
+    private double precio;
+
+    public Product() {
+        codigo = "";
+        descripcion = "";
+        precio = 0;
+    }
+
+    public Product(String codigo, String descripcion, double precio) {
+        this.codigo      = codigo;
+        this.descripcion = descripcion;
+        this.precio      = precio;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Product.class.getSimpleName() + "[", "]")
+            .add("codigo='" + codigo + "'")
+            .add("descripcion='" + descripcion + "'")
+            .add("precio=" + precio)
+            .toString();
+    }
+}
